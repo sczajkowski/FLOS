00bcb070dab1b811a0d131142f76058f7b1b2b41
Filip Tuliszkiewicz <filiptuliszkiewicz@Filips-MacBook-Pro.local>
2021-12-07 21:09:41 +0100

    Order redirect to order blade

--- FlosV1/flos/resources/views/user.blade.php
+++ FlosV1/flos/resources/views/user.blade.php
@@ -69,17 +69,13 @@
                     </div>
                     @foreach($orders as $order)
                         <div class="col-lg-4">
-                            <a class="card bg-primary btn" type="button" data-bs-toggle="modal" data-bs-target="#orderModal">
+                            <a class="card bg-primary btn" type="button" href="{{route('order',[$user, $order->orderId])}}">
                                 <div class="card-header border-0">
                                     <h3 class="card-title">{{$order->table}}</h3>
                                 </div>
                             </a>
                         </div>
                     @endforeach
-
-
-
-
                     <!-- /.col-md-6 -->
                     <!-- /.col-md-6 -->
                 </div>
